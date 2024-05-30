ce73ec17f38d1b27a156ea068103293ef60401e5
Levia <cr1o0127@gmail.com>
2024-05-30 14:31:58 +0800

    feat: chatroom style

--- src/components/ChatBox/index.tsx
+++ src/components/ChatBox/index.tsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+import { Box, Chat, Input, Message, Wrapper } from "./style";
+
 function ChatBox() {
-  return <></>;
+  const [text, setText] = useState("");
+  const [chat, setChat] = useState<string[]>([]);
+  return (
+    <Wrapper>
+      <Chat>
+        <Box>
+          {chat.map((el, i) => (
+            <Message key={i} $idenity={false}>
+              <p>{el}</p>
+            </Message>
+          ))}
+        </Box>
+        <Input>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setChat((prev) => [...prev, text]);
+                setText("");
+              }
+            }}
+          />
+        </Input>
+      </Chat>
+    </Wrapper>
+  );
 }
 
 export default ChatBox;
--- src/components/ChatBox/style.ts
+++ src/components/ChatBox/style.ts
@@ -1,8 +1,68 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Wrapper = styled.div`
+interface Message {
+  readonly $idenity: boolean;
+}
+
+export const Wrapper = styled.section`
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   height: 100vh;
 `;
 
-export const Box = styled.div``;
+export const Chat = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 700px;
+  height: 500px;
+  border: 1px solid #000;
+`;
+
+export const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  justify-content: end;
+  gap: 8px;
+  overflow: scroll;
+  width: 100%;
+  padding: 10px;
+`;
+
+export const Input = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 10px 0;
+  input {
+    width: 80%;
+    padding: 4px 8px;
+    border: 1px solid #000;
+    border-radius: 40px;
+    font-size: 16px;
+  }
+`;
+
+const current = css`
+  align-self: flex-end;
+`;
+
+const remote = css`
+  align-self: flex-start;
+  background-color: #000;
+  color: #fff;
+`;
+
+export const Message = styled.div<Message>`
+  display: block;
+  overflow-wrap: break-word;
+  max-width: 400px;
+  padding: 4px 16px;
+  border: 1px solid #000;
+  border-radius: 20px;
+  transition: all 0.3s;
+
+  ${({ $idenity }) => ($idenity ? current : remote)}
+`;
