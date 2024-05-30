47d793a3c905610dd545950652362f6faecb3fdc
Levia <cr1o0127@gmail.com>
2024-05-30 22:08:03 +0800

    chore: add chat

--- src/components/ChatBox/index.tsx
+++ src/components/ChatBox/index.tsx
@@ -1,16 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Chat, Input, Message, Wrapper } from "./style";
+import { child, onValue, push } from "firebase/database";
+import { dbRef } from "../../firebase/firebase";
+
+interface chat {
+  user: string;
+  text: string;
+}
 
 function ChatBox() {
+  const roomRef = child(dbRef, "room1");
+  const [user, setUser] = useState("");
   const [text, setText] = useState("");
-  const [chat, setChat] = useState<string[]>([]);
+  const [chat, setChat] = useState<chat[]>([]);
+  useEffect(() => {
+    const userName = prompt("請輸入名稱");
+    setUser(userName || "");
+    onValue(roomRef, (snap) => {
+      const data = snap.val();
+      const chatList = Object.values(data);
+      setChat(chatList as chat[]);
+    });
+  }, []);
+  function sendMessage() {
+    const data = { user, text };
+    push(roomRef, data);
+  }
+
   return (
     <Wrapper>
       <Chat>
         <Box>
           {chat.map((el, i) => (
-            <Message key={i} $idenity={true}>
-              <p>{el}</p>
+            <Message key={i} $idenity={el.user === user}>
+              <p>{el.text}</p>
             </Message>
           ))}
         </Box>
@@ -23,8 +46,8 @@ function ChatBox() {
             }}
             onKeyDown={(e) => {
               if (e.key === "Enter") {
-                setChat((prev) => [...prev, text]);
                 setText("");
+                sendMessage();
               }
             }}
           />
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,9 +1,10 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEYS,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIRESBASE_MESSAGING_SENDERID,
@@ -12,5 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
-export const db = getDatabase(app);
+export const dbRef = ref(db);
